dd6d3e2eccce4b72d172ecaa0000046140907608
igorbronk11 <rabota.it.russia@gmail.com>
2025-12-19 20:19:17 +0700

    Добавил замечания в код

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+
+// TODO это то что ниже что такое? Тоже удалить
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        /* TODO: Нужно добавить считывание из консоли и зациклить при получении
+            отрицательного значения массива
+         */
         Library.checkSize(-2);
         Library.checkSize(3);
         Library.checkSize(0);
         Library.checkSize(-1);
         Library.randomArray(8, 41, 999);
         Library library = new Library();
+
+        // TODO: что значит a? это что такое?
         int[] a = {6, 54, 21, 85, 2, 185, 3, 48};
+        // TODO: нужно вызвать на массиве который сгенерируется
         library.arrayMin(a);
     }
 }

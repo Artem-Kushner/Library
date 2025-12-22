4a489db2ec682bdcce3da29fb6890857e77d19cd
igorbronk11 <rabota.it.russia@gmail.com>
2025-12-22 13:54:30 +0700

    Удалил лишние TODO и комментарии

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// TODO это то что ниже что такое? Тоже удалить
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,23 +29,6 @@ public class Main {
             library.maxIndex(array);
             System.out.println("Индекс максимально элемента масива: " + library.getIntMax());
         }
-
-        /* TODO: Нужно добавить считывание из консоли и зациклить при получении
-            отрицательного значения массива
-
-        Library.checkSize(-2);
-        Library.checkSize(3);
-        Library.checkSize(0);
-        Library.checkSize(-1);
-        Library.randomArray(8, 41, 999);
-        Library library = new Library();
-
-        // TODO: что значит a? это что такое?
-        int[] a = {6, 54, 21, 85, 2, 185, 3, 48};
-        // TODO: нужно вызвать на массиве который сгенерируется
-        library.arrayMin(a);
-
-         */
     }
 }
 
